cea8cdcbd3a05c338abe30829053ad4b18e02004
arvin <li3741@qq.com>
2017-09-07 18:46:15 +0800

    建立项目
    匆匆开始，估计又是匆匆结束

---
+++ KungHua/KungHua.WeChat/WeChatConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KungHua.WeChat
+{
+    /// <summary>
+    /// 全局配置
+    /// </summary>
+    public class WeChatConfig
+    {
+        #region 全局配置的单例模式
+        private static WeChatConfig _weChatConfig;
+        private static string locker;
+        public static WeChatConfig()
+        {
+            locker = "wechatconfig";
+        }
+        public static WeChatConfig Ins()
+        {
+            if (_weChatConfig == null)
+            {
+                lock (locker)
+                {
+                    if (_weChatConfig == null)
+                    {
+                        _weChatConfig = new WeChatConfig();
+                    }
+                }
+            }
+            return _weChatConfig;
+        }
+        #endregion
+
+
+
+        #region 属性
+        public string AppId { get; set; }
+        public string Appsecret { get; set; }
+        public string Token { get; set; }
+        #endregion
+    }
+}
---
+++ KungHua/KungHua.WeChat/WeChatUrl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KungHua.WeChat
+{
+    /// <summary>
+    /// 定义微信通用常量url,即基本请求url
+    /// </summary>
+    public class WeChatUrl
+    {
+        /// <summary>
+        /// access_token是公众号的全局唯一接口调用凭据，公众号调用各接口时都需使用access_token。
+        /// 开发者需要进行妥善保存。access_token的存储至少要保留512个字符空间。
+        /// access_token的有效期目前为2个小时，需定时刷新，重复获取将导致上次获取的access_token失效。
+        /// </summary>
+        public static readonly string ACCESS_TOKEN = @"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}";
+
+    }
+}
